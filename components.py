01987aa46bd78121950d6f2acb8b409cbfb7b2c9
user1491040 <germanguzmanspencer@gmail.com>
2026-06-05 17:14:23 -0400

    Update components.py
    con pines de los demas componentes restantes

--- components.py
+++ components.py
@@ -1,30 +1,71 @@
 from libraries import LedRGB, Servo, Ultrasound, Infrared, WalkerBotV1
 
+# -------------------------
 # Motores
+# -------------------------
+
 SERVO_FR_PIN = 45
 SERVO_FL_PIN = 9
 SERVO_BR_PIN = 48
 SERVO_BL_PIN = 10
 
-# LED RGB
-RGB_RED_PIN = 5
-RGB_GREEN_PIN = 6
-RGB_BLUE_PIN = 7
+# -------------------------
+# Sensores
+# -------------------------
 
-# Ultrasonido
-US_TRIGGER_PIN = 4
-US_ECHO_PIN = 11
+US_TRIGGER_PIN = 1
+US_ECHO_PIN = 2
 
-# Infrarrojo
-INFRARED_PIN = 12
+IR_IZQ_PIN = 3
+IR_DER_PIN = 4
 
-motor_frontal_der = Servo(SERVO_FR_PIN, continuous=True)
-motor_frontal_izq = Servo(SERVO_FL_PIN, continuous=True)
-motor_trasero_der = Servo(SERVO_BR_PIN, continuous=True)
-motor_trasero_izq = Servo(SERVO_BL_PIN, continuous=True)
+# -------------------------
+# Inicialización de motores
+# -------------------------
 
-led = LedRGB(RGB_RED_PIN, RGB_GREEN_PIN, RGB_BLUE_PIN)
-detector_us = Ultrasound(US_TRIGGER_PIN, US_ECHO_PIN)
-detector_ir = Infrared(INFRARED_PIN)
+motor_frontal_der = Servo(
+    SERVO_FR_PIN,
+    continuous=True
+)
+
+motor_frontal_izq = Servo(
+    SERVO_FL_PIN,
+    continuous=True
+)
+
+motor_trasero_der = Servo(
+    SERVO_BR_PIN,
+    continuous=True
+)
+
+motor_trasero_izq = Servo(
+    SERVO_BL_PIN,
+    continuous=True
+)
+
+# -------------------------
+# Sensor ultrasónico frontal
+# -------------------------
+
+detector_us = Ultrasound(
+    US_TRIGGER_PIN,
+    US_ECHO_PIN
+)
+
+# -------------------------
+# Sensores infrarrojos laterales
+# -------------------------
+
+detector_ir_izq = Infrared(
+    IR_IZQ_PIN
+)
+
+detector_ir_der = Infrared(
+    IR_DER_PIN
+)
+
+# -------------------------
+# Control remoto WalkerBot
+# -------------------------
 
 control = WalkerBotV1()
